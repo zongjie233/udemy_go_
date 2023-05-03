df2a4f50edc48b4b655a763aec472644d9a69a92
zongjie233 <840573195@qq.com>
2023-05-03 22:46:06 +0800

    5.3 使用layout模板

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,7 @@ import (
 
 // RenderTemplate  渲染模板函数
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl) // 模板路径
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base_layout.tmpl") // 如果使用layout，则后边加上layout的文件位置
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
