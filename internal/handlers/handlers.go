d2b0f821b6a6fe8fa099bc38695e47641a58a6df
zongjie233 <840573195@qq.com>
2023-06-15 08:19:40 +0800

    6.14 helpers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/zongjie233/udemy_lesson/internal/config"
 	"github.com/zongjie233/udemy_lesson/internal/forms"
+	"github.com/zongjie233/udemy_lesson/internal/helpers"
 	"github.com/zongjie233/udemy_lesson/internal/models"
 	"github.com/zongjie233/udemy_lesson/internal/render"
 	"log"
@@ -33,25 +34,14 @@ func NewHandlers(r *Repository) {
 
 // Home is the Home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) { // 必须有着两个参数
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP) // 将访问ip存入session中，key值为”remote_ip“
-
 	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) { // 必须有着两个参数
-	// 业务逻辑
-	stringMap := make(map[string]string)
-	stringMap["test"] = "hello,world"
-
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
 	// 向模板发送数据
-	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
-		StringMap: stringMap,
-	})
+	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{})
 }
 
 // Reservation 渲染预定页面，展示表单
@@ -66,6 +56,7 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
+		helpers.ServerError(w, err)
 		log.Println(err)
 		return
 	}
@@ -131,7 +122,7 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	resp := jsonResponse{OK: true, Message: "Availability"}
 	out, err := json.MarshalIndent(resp, "", "    ")
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
 	}
 	w.Header().Set("Content-Type", "application/json") // 使客户端能够正确判断和解析服务器返回的数据格式
 	w.Write(out)
--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,9 +1,25 @@
 package helpers
 
-import "github.com/zongjie233/udemy_lesson/internal/config"
+import (
+	"fmt"
+	"github.com/zongjie233/udemy_lesson/internal/config"
+	"net/http"
+	"runtime/debug"
+)
 
 var app *config.AppConfig
 
 func NewHelpers(a *config.AppConfig) {
 	app = a
+}
+
+func ClientError(w http.ResponseWriter, status int) {
+	app.InfoLog.Println("Client error with status of", status)
+	http.Error(w, http.StatusText(status), status)
+}
+
+func ServerError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.ErrorLog.Println(trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
