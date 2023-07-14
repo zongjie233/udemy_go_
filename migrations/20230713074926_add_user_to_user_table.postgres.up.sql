6af49e3dcb57e80c28d59ef8c4741b79b87104f6
zongjie233 <840573195@qq.com>
2023-07-14 14:32:37 +0800

    7.14 formatting

---
+++ migrations/20230713074926_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,6 @@
+INSERT
+INTO
+    public.users
+(first_name, last_name, email, password, access_level, created_at, updated_at)
+VALUES
+    ('admin', '', 'admin@admin.com', '$2a$12$2QxyjhMQMx4JiYy3XalliOYdpQephzWhwmsaTpWBqKClcdXS1YhzS', 3, '2023-07-03 23:01:03.000000', '2023-07-03 23:01:05.000000');
